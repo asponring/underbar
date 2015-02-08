e45aa78203c32cff718d8623c6d8168d3f130c44
Andy Sponring <asponring@gmail.com>
2015-02-08 14:58:28 -0800

    Completed _.filter and _.reject

--- src/underbar.js
+++ src/underbar.js
@@ -80,12 +80,25 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var returnArr = [];
+
+    _.each(collection, function(item) {
+      if (test(item)) {
+        returnArr.push(item);
+      }
+    });
+
+    return returnArr;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+
+    return _.filter(collection, function(item) {
+      return !(test(item));
+    });
   };
 
   // Produce a duplicate-free version of the array.
