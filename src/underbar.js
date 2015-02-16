637e2142361a8d375ee00687a3f5b1d8a18b7d92
Andy Sponring <asponring@gmail.com>
2015-02-15 23:17:24 -0800

    Finished defaults()# Please enter the commit message for your changes. Lines starting

--- src/underbar.js
+++ src/underbar.js
@@ -250,6 +250,14 @@
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      _.each(arguments[i], function(item, key) {
+        if (obj[key] === undefined) {
+          obj[key] = item;
+        }
+      });
+    }
+    return obj;
   };
 
 
